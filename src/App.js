08a60cbb8bd58fc1f37e1448284041f1b2483c9d
ifreelander <roya.taleghani@gmail.com>
2020-05-29 16:17:17 +1000

    Base container and some initial styling added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import New from './Components/New.js';
+import List from './Components/List.js';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <header className="nav">
+          <Link to="/" className="navItem">
+            Home
+          </Link>
+          <Link to="/new" className="navItem">
+            New
+          </Link>
+        </header>
+      </div>
+
+      <Switch>
+        <Route path="/" exact component={List}></Route>
+        <Route path="/new" exact component={New}></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = () => {
+  return (
+    <div className="container">
+      <div className="body">List page showing published and drafts posts.</div>
+    </div>
+  );
+};
+
+export default List;
